02c5fcb7455a3120afb99b84d60a00826176b985
Mariomedhat899 <mariomedhat899@gmail.com>
2025-10-01 10:29:45 +0300

    Implementaion Of DAL

---
+++ Company.G02.DAL/Data/Contexts/CompanyDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Company.G02.DAL.Modles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Data.Contexts
+{
+    public class CompanyDBContext : DbContext
+    {
+
+        public CompanyDBContext() : base()
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= . ; Database = CompanyG02 Trusted_Connection = True ; TrustServerCertificate = True");
+        }
+
+       public DbSet<Department> departments { get; set; }
+        }
+}
